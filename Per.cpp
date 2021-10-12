acad2732b3b129ffc94ab54f05fecc2cb6499bf9
BhushanPTopre <82966990+BhushanPTopre@users.noreply.github.com>
2021-10-12 18:11:44 +0530

    Add files via upload

---
+++ Per.cpp
@@ -0,0 +1,18 @@
+//wap to demo first c++ code
+#include<iostream.h>
+#include<conio.h>
+class Per
+{
+       public:
+                    int x;
+                    void display()
+                    {
+                        cout<<"\n\nThe value of x is "<<x;
+                    }  
+};
+void main()
+{
+    Per p;
+    clrscr();
+    p.display();
+} 
